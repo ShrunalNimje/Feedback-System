b515e0fe11992a41e2f21816738ea5e50bad47c1
Shrunal Nimje <shrunalnimje@gmail.com>
2025-09-05 13:59:43 +0530

    created controller for student entity

---
+++ src/main/java/com/mymood/feedbacksystem/Feedback/System/Controller/StudentController.java
@@ -0,0 +1,52 @@
+package com.mymood.feedbacksystem.Feedback.System.Controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.mymood.feedbacksystem.Feedback.System.DTO.Request.StudentRequestDTO;
+import com.mymood.feedbacksystem.Feedback.System.DTO.Response.StudentResponseDTO;
+import com.mymood.feedbacksystem.Feedback.System.Service.StudentService;
+
+@RestController
+public class StudentController {
+
+	@Autowired
+    private StudentService studentService;
+
+    @PostMapping("/student")
+    public ResponseEntity<String> createStudent(@RequestBody StudentRequestDTO request) {
+        StudentResponseDTO saved = studentService.createStudent(request);
+        return ResponseEntity.ok("Student created successfully with id = " + saved.getStudentId());
+    }
+
+    @GetMapping("/student/{studentId}")
+    public StudentResponseDTO getStudent(@PathVariable Long studentId) {
+        return studentService.getStudent(studentId);
+    }
+
+    @GetMapping("/student")
+    public List<StudentResponseDTO> getAllStudents() {
+        return studentService.getAllStudent();
+    }
+
+    @PutMapping("/student/{studentId}")
+    public ResponseEntity<String> updateStudent(@PathVariable Long studentId, @RequestBody StudentRequestDTO update) {
+    	studentService.updateStudent(studentId, update);
+        return ResponseEntity.ok("Student updated successfully with id = " + studentId);
+    }
+
+    @DeleteMapping("/student/{studentId}")
+    public ResponseEntity<String> deleteStudent(@PathVariable Long studentId) {
+        studentService.deleteStudent(studentId);
+        return ResponseEntity.ok("Student deleted successfully with id = " + studentId);
+    }
+}
