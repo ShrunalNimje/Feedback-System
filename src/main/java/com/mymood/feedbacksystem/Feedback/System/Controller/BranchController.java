e825aba2ec487167574264d3f7fc9be966ef08dc
Shrunal Nimje <shrunalnimje@gmail.com>
2025-09-06 13:18:55 +0530

    created controller for branch entity

--- src/main/java/com/mymood/feedbacksystem/Feedback/System/Controller/BranchController.java
+++ src/main/java/com/mymood/feedbacksystem/Feedback/System/Controller/BranchController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -33,7 +34,7 @@ public class BranchController {
 		return ResponseEntity.ok("Branch deleted successfully with id = " + branchId);
 	}
 	
-	@PostMapping("/branch/{branchId}")
+	@PutMapping("/branch/{branchId}")
 	public ResponseEntity<String> updateBranch(@RequestBody BranchRequestDTO update, @PathVariable Long branchId) {
 		branchService.updateBranch(branchId, update);
 		return ResponseEntity.ok("Branch updated successfully with id = " + branchId);
