3b4ad20d7a0d9675d0efaafd4f50e194264e980d
Shrunal Nimje <shrunalnimje@gmail.com>
2025-09-06 13:20:51 +0530

    created controller for user entity

---
+++ src/main/java/com/mymood/feedbacksystem/Feedback/System/Controller/UserController.java
@@ -0,0 +1,57 @@
+package com.mymood.feedbacksystem.Feedback.System.Controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.mymood.feedbacksystem.Feedback.System.DTO.Request.UserRequestDTO;
+import com.mymood.feedbacksystem.Feedback.System.DTO.Response.UserResponseDTO;
+import com.mymood.feedbacksystem.Feedback.System.Service.UserService;
+
+@RestController
+public class UserController {
+
+	@Autowired
+    private UserService userService;
+
+    @PostMapping("/user")
+    public ResponseEntity<String> createUser(@RequestBody UserRequestDTO request) {
+        UserResponseDTO saved = userService.createUser(request);
+        return ResponseEntity.ok("User created successfully with id = " + saved.getUserId());
+    }
+
+    @GetMapping("/user/{userId}")
+    public UserResponseDTO getUserById(@PathVariable Long userId) {
+        return userService.getUserById(userId);
+    }
+
+    @GetMapping("/user/username/{username}")
+    public UserResponseDTO getUserByUsername(@PathVariable String username) {
+        return userService.getUserByUsername(username);
+    }
+
+    @GetMapping("/user")
+    public List<UserResponseDTO> getAllUsers() {
+        return userService.getAllUser();
+    }
+
+    @PutMapping("/user/{userId}")
+    public ResponseEntity<String> updateUser(@PathVariable Long userId, @RequestBody UserRequestDTO update) {
+        userService.updateUser(userId, update);
+        return ResponseEntity.ok("User updated successfully with id = " + userId);
+    }
+
+    @DeleteMapping("/user/{userId}")
+    public ResponseEntity<String> deleteUser(@PathVariable Long userId) {
+        userService.deleteUser(userId);
+        return ResponseEntity.ok("User deleted successfully with id = " + userId);
+    }
+}
