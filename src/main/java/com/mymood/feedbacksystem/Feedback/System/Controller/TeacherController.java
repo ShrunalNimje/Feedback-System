9559ba50f917792bcbae539e9db8be6e56fa2ce9
Shrunal Nimje <shrunalnimje@gmail.com>
2025-09-06 13:20:27 +0530

    created controller for teacher entity

---
+++ src/main/java/com/mymood/feedbacksystem/Feedback/System/Controller/TeacherController.java
@@ -0,0 +1,52 @@
+package com.mymood.feedbacksystem.Feedback.System.Controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.mymood.feedbacksystem.Feedback.System.DTO.Request.TeacherRequestDTO;
+import com.mymood.feedbacksystem.Feedback.System.DTO.Response.TeacherResponseDTO;
+import com.mymood.feedbacksystem.Feedback.System.Service.TeacherService;
+
+@RestController
+public class TeacherController {
+
+	@Autowired
+    private TeacherService teacherService;
+
+    @PostMapping("/teacher")
+    public ResponseEntity<String> createTeacher(@RequestBody TeacherRequestDTO request) {
+        TeacherResponseDTO saved = teacherService.createTeacher(request);
+        return ResponseEntity.ok("Teacher created successfully with id = " + saved.getTeacherId());
+    }
+
+    @GetMapping("/teacher/{teacherId}")
+    public TeacherResponseDTO getTeacher(@PathVariable Long teacherId) {
+        return teacherService.getTeacher(teacherId);
+    }
+
+    @GetMapping("/teacher")
+    public List<TeacherResponseDTO> getAllTeachers() {
+        return teacherService.getAllTeacher();
+    }
+
+    @PutMapping("/teacher/{teacherId}")
+    public ResponseEntity<String> updateTeacher(@PathVariable Long teacherId, @RequestBody TeacherRequestDTO update) {
+        teacherService.updateTeacher(teacherId, update);
+        return ResponseEntity.ok("Teacher updated successfully with id = " + teacherId);
+    }
+
+    @DeleteMapping("/teacher/{teacherId}")
+    public ResponseEntity<String> deleteTeacher(@PathVariable Long teacherId) {
+        teacherService.deleteTeacher(teacherId);
+        return ResponseEntity.ok("Teacher deleted successfully with id = " + teacherId);
+    }
+}
