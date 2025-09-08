de0819116326a3709f2b01595ae4b3b05383c81e
Shrunal Nimje <shrunalnimje@gmail.com>
2025-09-08 13:15:09 +0530

    created login dtos for users

---
+++ src/main/java/com/mymood/feedbacksystem/Feedback/System/DTO/Response/LoginResponseDTO.java
@@ -0,0 +1,23 @@
+package com.mymood.feedbacksystem.Feedback.System.DTO.Response;
+
+public class LoginResponseDTO {
+    
+	private String token;
+
+	public LoginResponseDTO() {
+		
+	}
+	
+    public LoginResponseDTO(String token) {
+        this.token = token;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+}
+
