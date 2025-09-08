4cedab93ab8581f8ff75392601419b105c34134f
Shrunal Nimje <shrunalnimje@gmail.com>
2025-09-08 13:15:39 +0530

    added JWT security

---
+++ src/main/java/com/mymood/feedbacksystem/Feedback/System/Security/CustomUserDetailsService.java
@@ -0,0 +1,26 @@
+package com.mymood.feedbacksystem.Feedback.System.Security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.mymood.feedbacksystem.Feedback.System.Entity.UserEntity;
+import com.mymood.feedbacksystem.Feedback.System.Repository.UserRepository;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService{
+
+	@Autowired
+	UserRepository userRepository;
+	
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		UserEntity user = userRepository.findByUsername(username)
+				.orElseThrow(() -> new UsernameNotFoundException("User not found!"));
+		
+		return new CustomUserDetails(user);
+	}
+
+}
