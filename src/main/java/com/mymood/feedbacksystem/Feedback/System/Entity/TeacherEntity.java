bccb5afb2bcee18fbdbe4a8c6b8065dbf91841de
Shrunal Nimje <shrunalnimje@gmail.com>
2025-09-10 18:08:58 +0530

    mapped user to teacher entity

--- src/main/java/com/mymood/feedbacksystem/Feedback/System/Entity/TeacherEntity.java
+++ src/main/java/com/mymood/feedbacksystem/Feedback/System/Entity/TeacherEntity.java
@@ -7,9 +7,9 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.Size;
 
 @Entity
 @Table(name = "teachers")
@@ -26,25 +26,25 @@ public class TeacherEntity {
 	@Email(message = "Please give a valid mail Id!")
 	private String email;
 	
-	@Size(min = 8, message = "Password should be of atleast 8 characters!")
-	@Column(unique = false, nullable = false)
-	private String password;
-	
 	@ManyToOne
 	@JoinColumn(name = "department_id", nullable = false)
 	private DepartmentEntity department;
+	
+	@OneToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private UserEntity user;
 	
 	public TeacherEntity() {
 		
 	}
 
-	public TeacherEntity(Long teacherId, String name, String email, String password, DepartmentEntity department) {
+	public TeacherEntity(Long teacherId, String name, String email, DepartmentEntity department, UserEntity user) {
 		super();
 		this.teacherId = teacherId;
 		this.name = name;
 		this.email = email;
-		this.password = password;
 		this.department = department;
+		this.user = user;
 	}
 
 	public Long getTeacherId() {
@@ -71,20 +71,20 @@ public class TeacherEntity {
 		this.email = email;
 	}
 
-	public String getPassword() {
-		return password;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
-	}
-
 	public DepartmentEntity getDepartment() {
 		return department;
 	}
 
 	public void setDepartment(DepartmentEntity department) {
 		this.department = department;
+	}
+
+	public UserEntity getUser() {
+		return user;
+	}
+
+	public void setUser(UserEntity user) {
+		this.user = user;
 	}
 
 }
